719bc25c514a2ca47adf9702550217627062e572
Roman Dubrovin <dubrovin.skfo@gmail.com>
2023-06-16 17:52:50 +0300

    переработал под fetch

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
-
+/*
 function createCardCountry(nameCountry) {
   const request = new XMLHttpRequest();
   request.open("GET", `https://restcountries.com/v3.1/name/${nameCountry}`);
@@ -48,3 +48,82 @@ createCardCountry("deutschland");
 createCardCountry("russia");
 createCardCountry("usa");
 createCardCountry("portugal");
+*/
+
+function renderCard(data, className = "") {
+  const languages = (lang) => {
+    const temp = [];
+    for (let key in lang) {
+      temp.push(lang[key]);
+    }
+    return temp.join(" ");
+  };
+
+  const currencies = (curr) => {
+    const temp = [];
+    for (let key in curr) {
+      temp.push(`${curr[key].name}`);
+    }
+    return temp.join("; ");
+  };
+
+  const html = `<article class="country ${className}">
+  <img class="country__img" src="${data.flags.svg}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${data.population}</p>
+    <p class="country__row"><span>🗣️</span>${languages(data.languages)}</p>
+    <p class="country__row"><span>💰</span>${currencies(data.currencies)}</p>
+  </div>
+</article>`;
+
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+}
+
+function createCardCountry(nameCountry) {
+  fetch(`https://restcountries.com/v3.1/name/${nameCountry}`)
+    .then(
+      (response) => response.json(),
+      (err) =>
+        countriesContainer.insertAdjacentText(
+          "beforeend",
+          `Произошла ошибка: ${err.message}. Попробуйте позже!`
+        )
+    )
+    .then((dataFromRes) => {
+      const [data] = dataFromRes;
+      renderCard(data);
+      if (data.borders.length > 0) createCardNeighbours(data);
+    })
+    .catch((err) =>
+      countriesContainer.insertAdjacentText(
+        "beforeend",
+        `Произошла ошибка: ${err.message}. Попробуйте позже!`
+      )
+    );
+}
+
+function createCardNeighbours(data) {
+  data.borders.forEach((code) => {
+    fetch(`https://restcountries.com/v3.1/alpha/${code}`)
+      .then((response) => response.json())
+      .then((dataFromRes) => {
+        const [data] = dataFromRes;
+        renderCard(data, "neighbour");
+      })
+      .catch((err) =>
+        countriesContainer.insertAdjacentText(
+          "beforeend",
+          `Произошла ошибка: ${err.message}. Попробуйте позже!`
+        )
+      );
+  });
+}
+
+btn.addEventListener("click", () => createCardCountry("russia"));
+//createCardCountry("russia");
+//createCardCountry("usa");
+//createCardCountry("deutschland");
+//createCardCountry("ukraine");
